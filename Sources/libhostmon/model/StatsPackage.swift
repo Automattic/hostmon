265eb111ab62a5f52ed89f8ee29a0d0a6a9f5774
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2023-01-19 22:28:11 -0700

    Move OperatingSystemVersion extension to Format

--- Sources/libhostmon/model/StatsPackage.swift
+++ Sources/libhostmon/model/StatsPackage.swift
@@ -109,21 +109,3 @@ public struct StatsPackage: Encodable {
         try container.encode(fanMap, forKey: .fanSpeeds)
     }
 }
-
-extension OperatingSystemVersion: Encodable {
-
-    enum CodingKeys: String, CodingKey {
-        case major, minor, patch
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(self.majorVersion, forKey: .major)
-        try container.encode(self.minorVersion, forKey: .minor)
-        try container.encode(self.patchVersion, forKey: .patch)
-    }
-
-    var asDouble: Double {
-        Double(self.patchVersion + self.minorVersion * 100 + self.majorVersion * 10_000)
-    }
-}
